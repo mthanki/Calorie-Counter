dabb01de0a36fdacc78bb5333e93d4fff1ba74b6
mthanki <60077334+mthanki@users.noreply.github.com>
2021-02-10 20:25:50 -0500

    Add project files.

---
+++ Calorie Counter/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calorie_Counter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int APPLE_CALORIES = 80;
+        const int BANANA_CALORIES = 115;
+        const int ORANGE_CALORIES = 90;
+        const int PEAR_CALORIES = 120;
+
+        int TotalCalories = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetTotalCaloriesInView()
+        {
+            TotalCaloriesLabel.Content = TotalCalories;
+        }
+
+        private void FruitImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Button = sender as Button;
+            var prop = Button.Tag;
+
+            switch (prop)
+            {
+                case "Apple":
+                    TotalCalories += APPLE_CALORIES;
+                    SetTotalCaloriesInView();
+                    break;
+                case "Banana":
+                    TotalCalories += BANANA_CALORIES;
+                    SetTotalCaloriesInView();
+                    break;
+                case "Orange":
+                    TotalCalories += ORANGE_CALORIES;
+                    SetTotalCaloriesInView();
+                    break;
+                case "Pear":
+                    TotalCalories += PEAR_CALORIES;
+                    SetTotalCaloriesInView();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ResetCaloriesTotal_Click(object sender, RoutedEventArgs e)
+        {
+            TotalCalories = 0;
+            SetTotalCaloriesInView();
+        }
+    }
+}
